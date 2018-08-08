9998871d661f24e38433bfd852220dd952f3c39b
Dftechteam <dftechteam@gmail.com>
2018-08-08 10:33:46 +0530

    Vijay

--- app/src/main/java/dfdashboardtest/com/myapplication/DETMainActivity.java
+++ app/src/main/java/dfdashboardtest/com/myapplication/DETMainActivity.java
@@ -1,13 +1,44 @@
 package dfdashboardtest.com.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DETMainActivity extends AppCompatActivity {
 
+
+    Button detfees_bt;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detmain);
+
+
+        detfees_bt =(Button)findViewById(R.id.detfees_BT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        detfees_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(DETMainActivity.this,HostelFeesActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
