3d88e2f398dc5968d2ed3554a45d65ad848146e0
Dftechteam <dftechteam@gmail.com>
2018-08-11 12:10:56 +0530

    Vijay

--- app/src/main/java/dfdashboardtest/com/myapplication/DETHRMS_Activity.java
+++ app/src/main/java/dfdashboardtest/com/myapplication/DETHRMS_Activity.java
@@ -1,9 +1,19 @@
 package dfdashboardtest.com.myapplication;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+//import android.support.design.widget.TabLayout;
 
-public class DETHRMS_Activity extends AppCompatActivity {
+public class DETHRMS_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,3 +21,39 @@ public class DETHRMS_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_dethrms);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dethrms);
+    }
+}*/
